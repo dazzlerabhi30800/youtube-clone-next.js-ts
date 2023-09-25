55c7bae2f2aa328d01e724edb106fc6cafe19c13
= <abhisingh30800@gmail.com>
2023-09-25 12:21:40 +0530

    added the hamburger

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -1,19 +1,24 @@
 import styles from '@/app/page.module.css';
 import { BsSearch } from 'react-icons/bs';
 import { BiBell } from 'react-icons/bi';
-import { AiOutlineVideoCamera } from 'react-icons/ai';
+import { AiOutlineVideoCamera, AiOutlineMenu } from 'react-icons/ai';
 
 import Image from "next/image";
 
 export default function Header() {
     return (
         <header>
-            <Image
-                src="/youtube.png"
-                height={25}
-                width={40}
-                alt="youtube"
-            />
+            <div className={styles.logoDiv}>
+                <button className={styles.hamburgerBtn}>
+                    <AiOutlineMenu />
+                </button>
+                <Image
+                    src="/youtube.png"
+                    height={25}
+                    width={40}
+                    alt="youtube"
+                />
+            </div>
             <form>
                 <input type="text" placeholder="Enter your search" />
                 <button className={styles.searchBtn}>
