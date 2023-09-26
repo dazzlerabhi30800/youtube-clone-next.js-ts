83074c9984cdbf444c6cd1b9f714a4b5cd0169d7
= <abhisingh30800@gmail.com>
2023-09-26 09:51:26 +0530

    added loading spinner

---
+++ app/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loader() {
+    return (
+        <div className="spinner--container">
+
+            <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+        </div>
+    )
+}
